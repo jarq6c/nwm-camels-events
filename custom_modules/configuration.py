f491fd212136ceb46915900a3b22e0895725a185
jarq6c <jason.regina@noaa.gov>
2025-05-01 14:01:43 -0500

    validate file mapping keys

--- custom_modules/configuration.py
+++ custom_modules/configuration.py
@@ -1,6 +1,7 @@
 """Handle project configuration file."""
 from pathlib import Path
-from pydantic import BaseModel, HttpUrl
+from typing import Annotated
+from pydantic import BaseModel, HttpUrl, AfterValidator
 from hydrotools.nwm_client.FileDownloader import FileDownloader
 
 class FileDetails(BaseModel):
@@ -19,6 +20,33 @@ class FileDetails(BaseModel):
     filename: str
     filepath: Path | None = None
 
+def validate_keys(
+        file_mapping: dict[str, FileDetails],
+        required: list[str] | None = None
+        ) -> dict[str, FileDetails]:
+    """
+    Validate keys in the file mapping.
+
+    Parameters
+    ----------
+    file_mapping : dict[str, FileDetails]
+        Dictionary containing file details.
+    required : list[str] | None
+        List of required keys to check in the file mapping.
+    
+    Returns
+    -------
+    dict[str, FileDetails]
+        Validated file mapping.
+    """
+    if required is None:
+        required = ["camels_list"]
+
+    for key in required:
+        if key not in file_mapping:
+            raise ValueError(f"Missing required file_mapping key: {key}")
+    return file_mapping
+
 class Config(BaseModel):
     """Configuration for downloading CAMELS basins data.
     
@@ -30,7 +58,7 @@ class Config(BaseModel):
         Mapping of file names to their download URLs and file paths.
     """
     data_dir: Path
-    file_mapping: dict[str, FileDetails]
+    file_mapping: Annotated[dict[str, FileDetails], AfterValidator(validate_keys)]
 
 def load_config(config_file: str) -> Config:
     """Load configuration from a JSON file.
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 """Process CAMELS bains."""
 from custom_modules.configuration import load_config, download_files
+
+import pandas as pd
 # from hydrotools.nwis_client.iv import IVDataService
 
 def main():
@@ -12,6 +14,15 @@ def main():
 
     # Download the CAMELS basins data
     config = download_files(config)
+
+    # Load CAMELS list
+    camels_list = pd.read_csv(
+        config.file_mapping["camels_list"].filepath,
+        sep=";",
+        dtype=str
+        )
+
+    print(camels_list)
 
 if __name__ == '__main__':
     # Call the main function to execute the script
